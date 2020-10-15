f3f1dadab09cab55c584676aa1fe2bc40e1812f2
JianiC <chenjiani930522@gmail.com>
2020-10-14 22:05:15 -0400

    acmap modify

--- cluster_map/antigentic_cartography.R
+++ cluster_map/antigentic_cartography.R
@@ -2,7 +2,7 @@
 # devtools::install_github("YuLab-SMU/ggacmap")
 #library(ggacmap)
 #remotes::install_url("https://github.com/acorg/acmacs.r/releases/download/v4.6/acmacs.r_4.6_R_macOS-10.14.tgz", build = FALSE)
-devtools::install_github("acorg/Racmacs")
+#devtools::install_github("acorg/Racmacs")
 library(Racmacs)
 map_file <- system.file("extdata/h3map2004.ace", package = "Racmacs")
 map <- read.acmap(map_file)
@@ -12,14 +12,12 @@ path_to_titer_file <- system.file("extdata/h3map2004_hitable.csv", package = "Ra
 
 test<-read.table("iVAX/RSVB_F_analysis_old/test2.csv",sep=",",  header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE,row.names = 1)
 
-titer_table <- read.titerTable("iVAX/RSVB_F_analysis_old/test2.csv")
+titer_table <-  read.titerTable(path_to_titer_file)
 
-test<-read.table("iVAX/RSVB_F_analysis_old/test4.txt",sep="\t",  header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE,row.names = 1)
 test<-read.table("Test_data/smith-2004-data.txt",sep="\t",  header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE,row.names = 1)
-
-test<-read.table("Test_data/epicc_test.txt",sep="\t",  header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE,row.names = 1)
+test2<-read.table("Test_data/epicc_test.txt",sep="\t",  header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE,row.names = 1)
 map <- acmap(
-  table = test
+  table = titer_table #test
 )
 
 map <- optimizeMap(
