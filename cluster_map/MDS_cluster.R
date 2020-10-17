843836a72c1241bc9c911f0a67a5e9d11cfc4a7a
JianiC <chenjiani930522@gmail.com>
2020-10-16 23:40:41 -0400

    add distance

--- cluster_map/MDS_cluster.R
+++ cluster_map/MDS_cluster.R
@@ -3,18 +3,20 @@
 # each row identified by a unique row name
 ## read the dataframe
 
-data<-read.table("Test_data/epicc_mds_test.csv",sep=",",row.names = 1,header=TRUE)
+data<-read.table("Test_data/smithetal_normshared_Oct15.csv",sep=",",row.names = 1,header=TRUE)
+
 library(magrittr)
 library(dplyr)
 library(ggpubr)
+library(purrr)
 
-# compute MDS
+# compute MDS ## GOF determine diminsion?
 mds<-data%>%
   dist() %>%
-  cmdscale() %>%
+  cmdscale(k=3) %>%
   as_tibble()
 
-colnames(mds)<-c("Dim.1","Dim.2")
+colnames(mds)<-c("Dim.1","Dim.2","Dim.3")
 
 # plot MDS
 ggscatter(mds, x = "Dim.1", y = "Dim.2", 
@@ -26,7 +28,7 @@ ggscatter(mds, x = "Dim.1", y = "Dim.2",
 
 ############################################################
 ## find the ooptimal number of cluster
-et.seed(123)
+set.seed(123)
 
 # function to compute total within-cluster sum of square 
 wss <- function(k) {
@@ -44,47 +46,110 @@ plot(k.values, wss_values,
      xlab="Number of clusters K",
      ylab="Total within-clusters sum of squares")
 
+## other approaches
+#install.packages("NbClust")
+#install.packages("factoextra")
+library(factoextra)
+library(NbClust)
+# Gap statistic
+# nboot = 50 to keep the function speedy. 
+# recommended value: nboot= 500 for your analysis.
+# Use verbose = FALSE to hide computing progression.
+set.seed(123)
+fviz_nbclust(mds, kmeans, nstart = 25,  method = "gap_stat", nboot = 50,k.max = 15)+
+  labs(subtitle = "Gap statistic method")
+
+# Silhouette method
+fviz_nbclust(mds, kmeans, method = "silhouette")+
+  labs(subtitle = "Silhouette method")
+
+# Elbow method
+fviz_nbclust(mds, kmeans, method = "wss") +
+  geom_vline(xintercept = 4, linetype = 2)+
+  labs(subtitle = "Elbow method")
 
 
+NbClust(data = mds, diss = NULL, distance = "euclidean",min.nc = 2, max.nc = 15, method = kmeans)
 ###########################################################
 
 
 # K-means clustering
-clust <- kmeans(mds, 2)$cluster %>%
+clust <- kmeans(mds,10)$cluster %>%
   as.factor()
 mds <- mds %>%
   mutate(groups = clust)
 # Plot and color by groups
-ggscatter(mds, x = "Dim.1", y = "Dim.2", 
-          #label = rownames(data),
-          color = "groups",
-          palette = "jco",
-          size = 3, 
-          ellipse = TRUE,
-          ellipse.type = "convex",
-          repel = TRUE)
+ggplot(mds, aes(x=Dim.1, y=Dim.2, color=groups)) + 
+  geom_point()+
+  scale_color_brewer(palette = "Set3",name="T-cell immuno-clusters") 
 
 
+
+######################################
+## plot tree by side
+library(treeio)
+library(ggplot2)
+library(ggtree)
+library(phytools)
+library(ape)
+library(RColorBrewer)
+library("dplyr")
+library("tibble")
+mds$strain<-rownames(data)
+t1 <- read.tree(file = "Test_data/Smith_tree/best_tree_smith_2004_HA1.nwk")
+p1<-ggtree(t1) 
+p1
+
+#write.csv(mds,"Test_data/smith_HA1_1015.csv",row.names = FALSE)
+cluster<-mds %>% select(strain,groups)
+#cluster<-column_to_rownames(cluster, 'strain')
+colnames(cluster)<-c("taxa","groups")
+#cluster <- read.table("Test_data/mds_group_test.csv", sep=",", header=TRUE,check.names=FALSE, stringsAsFactor=F, fill=TRUE)
+cluster$taxa<- gsub('_', '/', cluster$taxa)
+cluster$groups<-as.factor(cluster$groups)
+p2<-p1 %<+% cluster+ 
+  geom_tippoint(aes(color=groups), size=2, alpha=.75)+
+  scale_color_brewer(palette = "Set3",name="T-cell immuno-clusters") 
+p2
 ##############################################
 #calculate the distance between two points
 # construct the mds table
+library('tidyverse')
+library('tidyr')
+library(dplyr)
 
-mds$strain<-rownames(data)
+#calculate distance, eg(distance to BI/16190/68)
+# x and y coord for reference point
+X0=-0.18335288
+Y0=-0.0782464947
+Z0=	0.011120043
+mds$dis<-sqrt((mds$Dim.1-X0)^2+(mds$Dim.2-Y0)^2+(mds$Dim.3-Z0)^2)
+# seperate the season from strain name
+mds<-mds%>%
+  separate(strain,into=c("antigentic_group","extra","season"))
 
-#calculate distance, eg(north vaccine1 to north vaccine 2020)
+# a function to translate year 
+foo <- function(x, year=1968){
+  
+  y <- ifelse(x >= year %% 100, 1900+x, 2000+x)
+  return(y)
+}
 
-dist()
+mds$test<-lapply(as.numeric(mds$season),foo)
+library(ggplot2)
+ggplot(mds, aes(x=as.numeric(test), y=dis,color=groups)) + 
+  geom_point()+
+  scale_colour_brewer(palette = "Set3",name="T-cell immuno-clusters")
+####################################################################3
+#use the relatness score
+mds$relate<-data$BI_16190_68
 
-
-
-
-
-
-
-
-
+ggplot(mds, aes(x=as.numeric(test), y=relate,color=groups)) + 
+  geom_point()+
+  scale_colour_brewer(palette = "Set3",name="T-cell immuno-clusters")
+###################################################################
 d <- dist(data) # euclidean distances between the rows
-fit <- cmdscale(d,eig=TRUE, k=2) # k is the number of dim
+fit <- cmdscale (d,eig=TRUE, k=2) # k is the number of dim
 fit # view results
 head(fit)
 # plot solution
